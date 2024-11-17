40f400bf90d79e781363505ea80f9e3d2b92f86f
wsdonald4 <wsdonald4@gmail.com>
2024-11-16 20:08:32 -0500

    creation of the bookbot python script

--- main.py
+++ main.py
@@ -2,8 +2,17 @@ def main():
     path = "books/frankenstein.txt"
     content = get_file_contents(path)
     words = count_words(content)
-    print(f"you have {words} in the document!")
-
+    count = dictionary(content)
+    print(f"--- Begin report of {path} ---")
+    print(f"{words} words found in the document")
+    print()
+    sorted_list = char_dict(count)
+    
+    for item in sorted_list:
+        if not item["ch"].isalpha():
+            continue
+        print(f"The '{item['ch']}' character was found '{item['num']}' times")
+    print("--- End report ---")
 
 def count_words(text):
     words = text.split()
@@ -12,6 +21,26 @@ def count_words(text):
 def get_file_contents(path):
     with open(path) as f:
         return f.read()
+    
+def sort_on(dict):
+    return dict["num"]
 
+def char_dict(dict):
+    sorted_list = []
+    for ch in dict:
+        sorted_list.append({"ch": ch, "num": dict[ch]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
+
+def dictionary(text):
+    lowered_words = text.lower()
+    diction = {}
+
+    for word in lowered_words:
+        if word in diction:
+            diction[word] += 1
+        else:
+            diction[word] = 1
+    return diction
 
 main()
